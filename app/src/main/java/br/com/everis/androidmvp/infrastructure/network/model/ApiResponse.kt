9ae09284e88ca3cbdbc4f23a8984d71b1bf88ff3
Bruno Santos <bruno.luiz88@gmail.com>
2020-06-14 11:32:47 -0300

    Update ApiResponse.kt

--- app/src/main/java/br/com/everis/androidmvp/infrastructure/network/model/ApiResponse.kt
+++ app/src/main/java/br/com/everis/androidmvp/infrastructure/network/model/ApiResponse.kt
@@ -1,6 +1,5 @@
 package br.com.everis.androidmvp.infrastructure.network.model
 
-//TODO: 1 - Mover esta classe para o package model
 data class ApiResponse(
     val message: String,
     val number: Int,
