2f67a3da5d652aa737e92c70f49fb9a496e07a4f
Bruno Santos <bruno.luiz88@gmail.com>
2020-06-14 11:33:05 -0300

    Update People.kt

--- app/src/main/java/br/com/everis/androidmvp/infrastructure/network/model/People.kt
+++ app/src/main/java/br/com/everis/androidmvp/infrastructure/network/model/People.kt
@@ -1,7 +1,6 @@
 package br.com.everis.androidmvp.infrastructure.network.model
 
 
-//TODO: 2 - Mover esta classe para o package model
 data class People(
     val craft : String,
     val name: String
